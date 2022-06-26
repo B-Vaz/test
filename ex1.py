9261bcbc726622eb8dfdb3670f662ac25f2c6d43
unknown <vazgen9630@gmail.com>
2022-06-26 10:29:04 +0400

    first commit

---
+++ ex1.py
@@ -0,0 +1,50 @@
+a = "Hello Vazgen"
+b = 'Hello Grish'
+k = " "
+c = a+k+b
+print (c)
+
+a = float(3.2)
+b = int(10)
+c = int(10)
+f = float(20.2)
+print(a + b + c - f)
+
+a = 3.2+1
+b = 10
+c = 20.2
+print (a+b)
+a = 3.2
+print (a+b)
+a = 'vazgen'
+b = 26.2
+print (a+str(b))
+
+a = 101
+print (a)
+def someFunction():
+ a = 'I am learning python'
+ print (a)
+someFunction()
+print (a)
+
+a = 1
+print (a)
+def any():
+ a = 'lalala'
+ print (a)
+any()
+print (a)
+any()
+del (a)
+any()
+a = 1;
+print (a)
+print (a)
+a = ('June' 'March' 'April')
+print (a)
+
+tup1 = ('Robert', 'Carlos','1965','Terminator 1995', 'Actor','Florida');
+tup2 = (1,2,3,4,5,6,7);
+print(tup1[0:3])
+print(tup2[1:5])
